995a7caaf524fee28a06de4c1bf78b6d737f25f9
prehawk <prehawk@localhost.localdomain>
2015-01-13 23:37:20 +0800

    OK~

--- doubanbook/doubanbook/items.py
+++ doubanbook/doubanbook/items.py
@@ -8,16 +8,26 @@
 import scrapy
 
 class GroupItem(scrapy.Item):
-	group_id = scrapy.Field()
-	user_count = scrapy.Field()
-	users = scrapy.Field() # a list of MemberItem
+    group_id = scrapy.Field()
+    user_count = scrapy.Field()
+    users = scrapy.Field() # a list of MemberItem
 
 class MemberItem(scrapy.Item):
-	user_id = scrapy.Field()
-	book_count = scrapy.Field()
-	books = scrapy.Field() # a list of bookid 
-	crawled = scrapy.Field()
+    user_id = scrapy.Field()
+    read    = scrapy.Field()
+    crawled = scrapy.Field()
+    # book_count = scrapy.Field()
+    # books = scrapy.Field() # a list of bookid 
+    # crawled = scrapy.Field()
+    
 
+class RateItem(scrapy.Item):
+    user_id = scrapy.Field()
+    book_id = scrapy.Field()
+    rate = scrapy.Field()
+    date = scrapy.Field()
+    tags = scrapy.Field()
+    comment = scrapy.Field()
 
 class BookItem(scrapy.Item):
-	info = scrapy.Field()
+    info = scrapy.Field()
--- doubanbook/doubanbook/pipelines.py
+++ doubanbook/doubanbook/pipelines.py
@@ -6,20 +6,56 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import pymongo
+import datetime
 from pymongo import MongoClient
 from scrapy.exceptions import DropItem
 
-conn = pymongo.Connection('localhost',27017) 
+from doubanbook.spiders import *
+from doubanbook.items import *
+
+
+conn = MongoClient('localhost',27017) 
+db = conn.group_mems
 
 class DoubanbookPipeline(object):
 
+    def __init__(self):
+        pass
+
     def process_item(self, item, spider):
-    	pass
+        if isinstance(item, MemberItem):
+            updoc = {"user_id":item['user_id'], "crawled":item['crawled'], "read":item['read'], "uptime": datetime.datetime.utcnow()}
+            db.users.update({"user_id":item['user_id']}, updoc, upsert=True)
+        elif isinstance(item, RateItem):
+            db.users.update({"user_id":item['user_id']}, {"$addToSet":{"history":dict(item)}})
+        elif isinstance(item, BookItem):
+            db.books.update({'book_id':item['info']['id']}, dict(item['info']), upsert=True)
 
     def open_spider(self, spider):
-    	db = conn.group_mems
-    	print db
-
+        users_in_db = db.users.find({})
+        if isinstance(spider, group_mems.GroupMemsSpider):   
+            for u in users_in_db:
+                spider.users.add(u['user_id'])
+            spider.prime_size = len(spider.users)
+        if isinstance(spider, user_books.UserBooksSpider):
+            for u in users_in_db: 
+                delta = datetime.datetime.utcnow() - u['uptime']
+                if u['crawled'] == 0 or ( delta.days >= 0 and u['read'] > 15 ):
+                    url = 'http://book.douban.com/people/%s/' % u['user_id']
+                    spider.start_urls.append(url)    
+                    if 'history' in u:
+                        #print 'collects %d/%d' % ( len(u['history']), u['read'] )
+                        spider.history[u['user_id']] = len(u['history'])
+                    elif u['crawled'] == 0:
+                        spider.history[u['user_id']] = -1
+                    else:
+                        #print ' start_users_read: %d' % u['read']
+                        spider.history[u['user_id']] = 0
+                    #print '%d, collects %d/%d' % (u['crawled'], spider.history[u['user_id']], u['read'])
+            print '=== start_urls: %d ===' % len(spider.start_urls)
+            books_in_db = db.books.find({}, {"id":1})
+            for b in books_in_db:
+                spider.books.add(b['id'])
 
     def close_spider(self, spider):
-    	pass
+        pass
